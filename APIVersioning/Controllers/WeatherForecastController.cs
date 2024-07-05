c8e05f9e82533afccf91fae556d4c354032ff09f
manishchopra@myself.com <manishchopra@myself.com>
2024-07-05 12:59:37 +0100

    removing summary from response

--- APIVersioning/Controllers/WeatherForecastController.cs
+++ APIVersioning/Controllers/WeatherForecastController.cs
@@ -41,8 +41,7 @@ namespace APIVersioning.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                TemperatureC = Random.Shared.Next(-20, 55)
             })
             .ToArray();
         }
